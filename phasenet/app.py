3371c9e2a2db0335dfb53bf8b39fc9f41f95adc5
Kaiwen Wang <kaiwenwang@Kaiwens-MacBook-Air.local>
2024-04-11 21:50:49 -0400

    Add existing file

---
+++ phasenet/app.py
@@ -0,0 +1,313 @@
+import os
+from collections import defaultdict, namedtuple
+from datetime import datetime, timedelta
+from json import dumps
+from typing import Any, AnyStr, Dict, List, NamedTuple, Union
+
+import numpy as np
+import requests
+import tensorflow as tf
+from fastapi import FastAPI
+from kafka import KafkaProducer
+from pydantic import BaseModel
+from scipy.interpolate import interp1d
+
+from model import ModelConfig, UNet
+from postprocess import extract_amplitude, extract_picks
+
+tf.compat.v1.disable_eager_execution()
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+PROJECT_ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
+JSONObject = Dict[AnyStr, Any]
+JSONArray = List[Any]
+JSONStructure = Union[JSONArray, JSONObject]
+
+app = FastAPI()
+X_SHAPE = [3000, 1, 3]
+SAMPLING_RATE = 100
+
+# load model
+model = UNet(mode="pred")
+sess_config = tf.compat.v1.ConfigProto()
+sess_config.gpu_options.allow_growth = True
+
+sess = tf.compat.v1.Session(config=sess_config)
+saver = tf.compat.v1.train.Saver(tf.compat.v1.global_variables())
+init = tf.compat.v1.global_variables_initializer()
+sess.run(init)
+latest_check_point = tf.train.latest_checkpoint(f"{PROJECT_ROOT}/model/190703-214543")
+print(f"restoring model {latest_check_point}")
+saver.restore(sess, latest_check_point)
+
+# GMMA API Endpoint
+GMMA_API_URL = "http://gmma-api:8001"
+# GMMA_API_URL = 'http://localhost:8001'
+
+# Kafak producer
+use_kafka = False
+
+try:
+    print("Connecting to k8s kafka")
+    BROKER_URL = "quakeflow-kafka-headless:9092"
+    producer = KafkaProducer(
+        bootstrap_servers=[BROKER_URL],
+        key_serializer=lambda x: dumps(x).encode("utf-8"),
+        value_serializer=lambda x: dumps(x).encode("utf-8"),
+    )
+    use_kafka = True
+    print("k8s kafka connection success!")
+except BaseException:
+    print("k8s Kafka connection error")
+    try:
+        print("Connecting to local kafka")
+        producer = KafkaProducer(
+            bootstrap_servers=["localhost:9092"],
+            key_serializer=lambda x: dumps(x).encode("utf-8"),
+            value_serializer=lambda x: dumps(x).encode("utf-8"),
+        )
+        use_kafka = True
+        print("local kafka connection success!")
+    except BaseException:
+        print("local Kafka connection error")
+print(f"Kafka status: {use_kafka}")
+
+
+def normalize_batch(data, window=3000):
+    """
+    data: nsta, nt, nch
+    """
+    shift = window // 2
+    nsta, nt, nch = data.shape
+
+    # std in slide windows
+    data_pad = np.pad(data, ((0, 0), (window // 2, window // 2), (0, 0)), mode="reflect")
+    t = np.arange(0, nt, shift, dtype="int")
+    std = np.zeros([nsta, len(t) + 1, nch])
+    mean = np.zeros([nsta, len(t) + 1, nch])
+    for i in range(1, len(t)):
+        std[:, i, :] = np.std(data_pad[:, i * shift : i * shift + window, :], axis=1)
+        mean[:, i, :] = np.mean(data_pad[:, i * shift : i * shift + window, :], axis=1)
+
+    t = np.append(t, nt)
+    # std[:, -1, :] = np.std(data_pad[:, -window:, :], axis=1)
+    # mean[:, -1, :] = np.mean(data_pad[:, -window:, :], axis=1)
+    std[:, -1, :], mean[:, -1, :] = std[:, -2, :], mean[:, -2, :]
+    std[:, 0, :], mean[:, 0, :] = std[:, 1, :], mean[:, 1, :]
+    std[std == 0] = 1
+
+    # ## normalize data with interplated std
+    t_interp = np.arange(nt, dtype="int")
+    std_interp = interp1d(t, std, axis=1, kind="slinear")(t_interp)
+    mean_interp = interp1d(t, mean, axis=1, kind="slinear")(t_interp)
+    data = (data - mean_interp) / std_interp
+
+    return data
+
+
+def preprocess(data):
+    raw = data.copy()
+    data = normalize_batch(data)
+    if len(data.shape) == 3:
+        data = data[:, :, np.newaxis, :]
+        raw = raw[:, :, np.newaxis, :]
+    return data, raw
+
+
+def calc_timestamp(timestamp, sec):
+    timestamp = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%f") + timedelta(seconds=sec)
+    return timestamp.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]
+
+
+def format_picks(picks, dt, amplitudes):
+    picks_ = []
+    for pick, amplitude in zip(picks, amplitudes):
+        for idxs, probs, amps in zip(pick.p_idx, pick.p_prob, amplitude.p_amp):
+            for idx, prob, amp in zip(idxs, probs, amps):
+                picks_.append(
+                    {
+                        "id": pick.fname,
+                        "timestamp": calc_timestamp(pick.t0, float(idx) * dt),
+                        "prob": prob,
+                        "amp": amp,
+                        "type": "p",
+                    }
+                )
+        for idxs, probs, amps in zip(pick.s_idx, pick.s_prob, amplitude.s_amp):
+            for idx, prob, amp in zip(idxs, probs, amps):
+                picks_.append(
+                    {
+                        "id": pick.fname,
+                        "timestamp": calc_timestamp(pick.t0, float(idx) * dt),
+                        "prob": prob,
+                        "amp": amp,
+                        "type": "s",
+                    }
+                )
+    return picks_
+
+
+def format_data(data):
+
+    # chn2idx = {"ENZ": {"E":0, "N":1, "Z":2},
+    #            "123": {"3":0, "2":1, "1":2},
+    #            "12Z": {"1":0, "2":1, "Z":2}}
+    chn2idx = {"E": 0, "N": 1, "Z": 2, "3": 0, "2": 1, "1": 2}
+    Data = NamedTuple("data", [("id", list), ("timestamp", list), ("vec", list), ("dt", float)])
+
+    # Group by station
+    chn_ = defaultdict(list)
+    t0_ = defaultdict(list)
+    vv_ = defaultdict(list)
+    for i in range(len(data.id)):
+        key = data.id[i][:-1]
+        chn_[key].append(data.id[i][-1])
+        t0_[key].append(datetime.strptime(data.timestamp[i], "%Y-%m-%dT%H:%M:%S.%f").timestamp() * SAMPLING_RATE)
+        vv_[key].append(np.array(data.vec[i]))
+
+    # Merge to Data tuple
+    id_ = []
+    timestamp_ = []
+    vec_ = []
+    for k in chn_:
+        id_.append(k)
+        min_t0 = min(t0_[k])
+        timestamp_.append(datetime.fromtimestamp(min_t0 / SAMPLING_RATE).strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3])
+        vec = np.zeros([X_SHAPE[0], X_SHAPE[-1]])
+        for i in range(len(chn_[k])):
+            # vec[int(t0_[k][i]-min_t0):len(vv_[k][i]), chn2idx[chn_[k][i]]] = vv_[k][i][int(t0_[k][i]-min_t0):X_SHAPE[0]] - np.mean(vv_[k][i])
+            shift = int(t0_[k][i] - min_t0)
+            vec[shift : len(vv_[k][i]) + shift, chn2idx[chn_[k][i]]] = vv_[k][i][: X_SHAPE[0] - shift] - np.mean(
+                vv_[k][i][: X_SHAPE[0] - shift]
+            )
+        vec_.append(vec.tolist())
+
+    return Data(id=id_, timestamp=timestamp_, vec=vec_, dt=1 / SAMPLING_RATE)
+    # return {"id": id_, "timestamp": timestamp_, "vec": vec_, "dt":1 / SAMPLING_RATE}
+
+
+def get_prediction(data, return_preds=False):
+
+    vec = np.array(data.vec)
+    vec, vec_raw = preprocess(vec)
+
+    shift=200
+    window=3000
+    nsta, nt, nch = data.shape
+    picks_all=[]
+    amps_all=[]
+    preds_all=[]
+    for i in range((nt-window)/shift+1):
+
+        vec_slidingwindow=vec[:,i*shift:i*shift+window,:,:]
+
+
+	feed = {model.X: vec_slidingwindow, model.drop_rate: 0, model.is_training: False}
+    	preds = sess.run(model.preds, feed_dict=feed)
+
+    	picks = extract_picks(preds, fnames=data.id, t0=data.timestamp)
+ 	amps = extract_amplitude(vec_raw, picks)
+	
+	picks_all.append(picks.tolist())
+        amps_all.append(amps.tolist())
+
+    #picks = format_picks(picks, data.dt, amps)
+    picks = format_picks(picks_all, data.dt, amps_all)
+
+
+    if return_preds:
+        return picks, preds_all
+    return picks
+
+
+class Data(BaseModel):
+    # id: Union[List[str], str]
+    # timestamp: Union[List[str], str]
+    # vec: Union[List[List[List[float]]], List[List[float]]]
+    id: List[str]
+    timestamp: List[str]
+    vec: Union[List[List[List[float]]], List[List[float]]]
+    dt: float = 0.01
+
+
+@app.get("/predict")
+def predict(data: Data):
+
+    picks = get_prediction(data)
+
+    return picks
+
+
+@app.get("/predict_prob")
+def predict(data: Data):
+
+    picks, preds = get_prediction(data, True)
+
+    return picks, preds.tolist()
+
+
+@app.get("/predict2gmma")
+def predict(data: Data):
+
+    picks = get_prediction(data)
+
+    if use_kafka:
+        for pick in picks:
+            producer.send("phasenet_picks", key=pick["id"], value=pick)
+    try:
+        catalog = requests.get(f"{GMMA_API_URL}/predict", json={"picks": picks})
+        print(catalog.json())
+        return catalog.json()
+    except Exception as error:
+        print(error)
+
+    return {}
+
+
+@app.get("/predict_seedlink")
+def predict(data: Data):
+
+    data = format_data(data)
+    # for i in range(len(data.id)):
+    #     plt.clf()
+    #     plt.subplot(311)
+    #     plt.plot(np.array(data.vec)[i, :, 0])
+    #     plt.subplot(312)
+    #     plt.plot(np.array(data.vec)[i, :, 1])
+    #     plt.subplot(313)
+    #     plt.plot(np.array(data.vec)[i, :, 2])
+    #     plt.savefig(f"{data.id[i]}.png")
+
+    picks = get_prediction(data)
+    print("PhaseNet:", picks)
+
+    return_value = {}
+    try:
+        catalog = requests.get(f"{GMMA_API_URL}/predict", json={"picks": picks})
+        print("GMMA:", catalog.json())
+        return_value = catalog.json()
+    except Exception as error:
+        print(error)
+
+    if use_kafka:
+        print("Push picks to kafka...")
+        for pick in picks:
+            producer.send("phasenet_picks", key=pick["id"], value=pick)
+        print("Push waveform to kafka...")
+        for id, timestamp, vec in zip(data.id, data.timestamp, data.vec):
+            producer.send("waveform_phasenet", key=id, value={"timestamp": timestamp, "vec": vec, "dt": data.dt})
+
+    return return_value
+
+
+@app.get("/test")
+def predict(data: JSONStructure):
+    # print(data)
+    # print(data)
+    # data = json.loads(data)
+    for row in data:
+        k, v = row
+        producer.send("waveform_raw", key=k, value=v)
+
+    # picks = get_prediction(data)
+
+    return {}
---
+++ phasenet/predict.py
@@ -0,0 +1,197 @@
+import argparse
+import logging
+import multiprocessing
+import os
+import sys
+
+n_CPUs = 1
+os.environ["OMP_NUM_THREADS"] = str(n_CPUs)
+
+import pickle
+import time
+from functools import partial
+
+import h5py
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tqdm import tqdm
+
+from data_reader import DataReader_mseed_array, DataReader_pred
+from model import ModelConfig, UNet
+from postprocess import (
+    extract_amplitude,
+    extract_picks,
+    save_picks,
+    save_picks_json,
+    save_prob_h5,
+)
+from visulization import plot_waveform
+
+tf.compat.v1.disable_eager_execution()
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+os.environ["TF_NUM_INTRAOP_THREADS"] = str(n_CPUs)
+os.environ["TF_NUM_INTEROP_THREADS"] = str(1)
+tf.config.threading.set_inter_op_parallelism_threads(1)
+tf.config.threading.set_intra_op_parallelism_threads(n_CPUs)
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+
+def read_args():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--batch_size", default=20, type=int, help="batch size")
+    parser.add_argument("--model_dir", help="Checkpoint directory (default: None)")
+    parser.add_argument("--data_dir", default="", help="Input file directory")
+    parser.add_argument("--data_list", default="", help="Input csv file")
+    parser.add_argument("--hdf5_file", default="", help="Input hdf5 file")
+    parser.add_argument("--hdf5_group", default="data", help="data group name in hdf5 file")
+    parser.add_argument("--result_dir", default="results", help="Output directory")
+    parser.add_argument("--result_fname", default="picks", help="Output file")
+    parser.add_argument("--highpass_filter", default=0.0, type=float, help="Highpass filter")
+    parser.add_argument("--min_p_prob", default=0.3, type=float, help="Probability threshold for P pick")
+    parser.add_argument("--min_s_prob", default=0.3, type=float, help="Probability threshold for S pick")
+    parser.add_argument("--mpd", default=50, type=float, help="Minimum peak distance")
+    parser.add_argument("--amplitude", action="store_true", help="if return amplitude value")
+    parser.add_argument("--format", default="numpy", help="input format")
+    parser.add_argument("--s3_url", default="localhost:9000", help="s3 url")
+    parser.add_argument("--stations", default="", help="seismic station info")
+    parser.add_argument("--plot_figure", action="store_true", help="If plot figure for test")
+    parser.add_argument("--save_prob", action="store_true", help="If save result for test")
+    args = parser.parse_args()
+
+    return args
+
+
+def pred_fn(args, data_reader, figure_dir=None, prob_dir=None, log_dir=None):
+    current_time = time.strftime("%y%m%d-%H%M%S")
+    if log_dir is None:
+        log_dir = os.path.join(args.log_dir, "pred", current_time)
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    if (args.plot_figure == True) and (figure_dir is None):
+        figure_dir = os.path.join(log_dir, 'figures')
+        if not os.path.exists(figure_dir):
+            os.makedirs(figure_dir)
+    if (args.save_prob == True) and (prob_dir is None):
+        prob_dir = os.path.join(log_dir, 'probs')
+        if not os.path.exists(prob_dir):
+            os.makedirs(prob_dir)
+    if args.save_prob:
+        h5 = h5py.File(os.path.join(args.result_dir, "result.h5"), "w", libver='latest')
+        prob_h5 = h5.create_group("/prob")
+    logging.info("Pred log: %s" % log_dir)
+    logging.info("Dataset size: {}".format(data_reader.num_data))
+
+    with tf.compat.v1.name_scope('Input_Batch'):
+        if args.format == "mseed_array":
+            batch_size = 1
+        else:
+            batch_size = args.batch_size
+        dataset = data_reader.dataset(batch_size)
+        batch = tf.compat.v1.data.make_one_shot_iterator(dataset).get_next()
+
+    config = ModelConfig(X_shape=data_reader.X_shape)
+    with open(os.path.join(log_dir, 'config.log'), 'w') as fp:
+        fp.write('\n'.join("%s: %s" % item for item in vars(config).items()))
+    if config.X_shape[0]>0:
+            
+        model = UNet(config=config, input_batch=batch, mode="pred")
+        # model = UNet(config=config, mode="pred")
+        sess_config = tf.compat.v1.ConfigProto()
+        sess_config.gpu_options.allow_growth = True
+        # sess_config.log_device_placement = False
+
+        with tf.compat.v1.Session(config=sess_config) as sess:
+
+            saver = tf.compat.v1.train.Saver(tf.compat.v1.global_variables(), max_to_keep=5)
+            init = tf.compat.v1.global_variables_initializer()
+            sess.run(init)
+
+            latest_check_point = tf.train.latest_checkpoint(args.model_dir)
+            logging.info(f"restoring model {latest_check_point}")
+            saver.restore(sess, latest_check_point)
+
+            picks = []
+            amps = [] if args.amplitude else None
+            if args.plot_figure:
+                multiprocessing.set_start_method('spawn')
+                pool = multiprocessing.Pool(multiprocessing.cpu_count())
+
+            for _ in tqdm(range(0, data_reader.num_data, batch_size), desc="Pred"):
+                if args.amplitude:
+                    pred_batch, X_batch, amp_batch, fname_batch, t0_batch, station_batch = sess.run(
+                        [model.preds, batch[0], batch[1], batch[2], batch[3], batch[4]],
+                        feed_dict={model.drop_rate: 0, model.is_training: False},
+                    )
+                #    X_batch, amp_batch, fname_batch, t0_batch = sess.run([batch[0], batch[1], batch[2], batch[3]])
+                else:
+                    pred_batch, X_batch, fname_batch, t0_batch, station_batch = sess.run(
+                        [model.preds, batch[0], batch[1], batch[2], batch[3]],
+                        feed_dict={model.drop_rate: 0, model.is_training: False},
+                    )
+                #    X_batch, fname_batch, t0_batch = sess.run([model.preds, batch[0], batch[1], batch[2]])
+                # pred_batch = []
+                # for i in range(0, len(X_batch), 1):
+                #     pred_batch.append(sess.run(model.preds, feed_dict={model.X: X_batch[i:i+1], model.drop_rate: 0, model.is_training: False}))
+                # pred_batch = np.vstack(pred_batch)
+
+                picks_ = extract_picks(preds=pred_batch, fnames=fname_batch, station_ids=station_batch, t0=t0_batch, config=args)
+                picks.extend(picks_)
+                if args.amplitude:
+                    amps_ = extract_amplitude(amp_batch, picks_)
+                    amps.extend(amps_)
+
+                if args.plot_figure:
+                    pool.starmap(
+                        partial(
+                            plot_waveform,
+                            figure_dir=figure_dir,
+                        ),
+                        zip(X_batch, pred_batch, [x.decode() for x in fname_batch]),
+                    )
+
+                if args.save_prob:
+                    # save_prob(pred_batch, fname_batch, prob_dir=prob_dir)
+                    save_prob_h5(pred_batch, [x.decode() for x in fname_batch], prob_h5)
+
+            save_picks(picks, args.result_dir, amps=amps, fname=args.result_fname+".csv")
+            save_picks_json(picks, args.result_dir, dt=data_reader.dt, amps=amps, fname=args.result_fname+".json")
+
+        print(
+            f"Done with {sum([len(x) for pick in picks for x in pick.p_idx])} P-picks and {sum([len(x) for pick in picks for x in pick.s_idx])} S-picks"
+        )
+    else:
+        print("data too short")
+    return 0
+
+
+def main(args):
+
+    logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+
+    with tf.compat.v1.name_scope('create_inputs'):
+
+        if args.format == "mseed_array":
+            data_reader = DataReader_mseed_array(
+                data_dir=args.data_dir, data_list=args.data_list, stations=args.stations, amplitude=args.amplitude
+            )
+        else:
+            data_reader = DataReader_pred(
+                format=args.format,
+                data_dir=args.data_dir,
+                data_list=args.data_list,
+                hdf5_file=args.hdf5_file,
+                hdf5_group=args.hdf5_group,
+                amplitude=args.amplitude,
+                highpass_filter=args.highpass_filter,
+            )
+
+        pred_fn(args, data_reader, log_dir=args.result_dir)
+
+    return
+
+
+if __name__ == '__main__':
+    args = read_args()
+    main(args)
